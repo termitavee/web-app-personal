9037b8a731e95b96927cd0fc54040a29832e43b3
termitavee <termitavee@gmail.com>
2021-10-11 17:36:02 +0200

    Add extra interest links

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -40,6 +40,8 @@ const Settings = () => {
 
   const onPressExpo = () => DeviceUtils.openUrl('https://expo.dev/@termitavee/jrdominguez');
 
+  const onPressSource = () => DeviceUtils.openUrl('https://github.com/termitavee/web-app-personal');
+
   return (
     <Container>
       <Title>{t('settings.description')}</Title>
@@ -98,7 +100,16 @@ const Settings = () => {
             Link to the
             <Text style={{ color: colors.primary }} onPress={onPressExpo}>
               {' '}
-              expo App
+              Expo App
+            </Text>
+          </Trans>
+        </Text>
+        <Text>
+          <Trans i18nKey="settings.linkToSource">
+            Link to the
+            <Text style={{ color: colors.primary }} onPress={onPressSource}>
+              {' '}
+              Source code
             </Text>
           </Trans>
         </Text>
