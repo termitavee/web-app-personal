1b3e514420309f8ab2eca131a2477af5f4176e27
termitavee <termitavee@gmail.com>
2022-03-13 19:34:08 +0100

    move all login inside src

--- App.tsx
+++ App.tsx
@@ -1,50 +1,8 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { ThemeProvider } from '@shopify/restyle';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Appearance, ColorSchemeName } from 'react-native';
-import { ToastProvider } from 'react-native-toast-notifications';
-
-import { darkTheme, lightTheme } from 'src/assets/themes';
-import { ContextProvider } from 'src/hooks/use-context';
-import { initialWindowMetrics, SafeAreaProvider } from 'src/hooks/use-safe-area';
-import Navigation from 'src/navigation/tab-navigation';
-import { useIsInit } from 'src/utils/translations';
-
-const styles = StyleSheet.create({
-  container: {
-    alignSelf: 'center',
-    width: '100%',
-    maxWidth: 500,
-    height: '100%',
-  },
-});
+import React from 'react';
+import Navigation from 'src/navigation/index';
 
 const App: React.FC = () => {
-  const [themeContext, setTheme] = useState<ColorSchemeName>(Appearance.getColorScheme());
-  const [currentTheme, setCurrentTheme] = useState(Appearance.getColorScheme() === 'dark' ? darkTheme : lightTheme);
-  const [loaded, setLoaded] = useState(false);
-  const isI19nInitialized = useIsInit();
-  const theme = (newTheme: ColorSchemeName) => {
-    setTheme(newTheme);
-    setCurrentTheme(newTheme === 'light' ? lightTheme : darkTheme);
-  };
-  useEffect(() => {
-    if (isI19nInitialized) setLoaded(true);
-  }, [isI19nInitialized]);
-  // linking={linking} ref={navigationRef}
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <ContextProvider value={{ themeContext, set: { theme } }}>
-        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-          <ToastProvider>
-            <NavigationContainer theme={currentTheme}>
-              <View style={styles.container}>{loaded && <Navigation />}</View>
-            </NavigationContainer>
-          </ToastProvider>
-        </SafeAreaProvider>
-      </ContextProvider>
-    </ThemeProvider>
-  );
+  return <Navigation />;
 };
 
 export default App;
